57042aa828639e8dcb16e999c45436d6f512444d
Macklemoon <m-moon@csu.fullerton.edu>
2021-03-31 23:26:43 -0700

    simplified add() and removed api key.

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -1,29 +1,25 @@
 (function (window) {
     'use strict';
     var config = {
-        apiKey: "AIzaSyAL6A27IoFwjxh1xxjZeO-a968SvHUxnzA",
-        projectId: "coffeeorder-7ef83",
+        projectId: "coffeeorder-7ef83"
     };
 
     var baseURL = "https://firestore.googleapis.com/v1/projects/coffeeorder-7ef83/databases/(default)/documents/coffeeorder";
 
     var App = window.App || {};
     var $ = window.jQuery;
-    var idToken;
 
     class RemoteDataStore {
         constructor() {
-            this.setID(config['apiKey']);
         }
 
         //--------------
         add(key, val) {
             $.ajax({
                 type: 'PATCH',
-                url: baseURL + key + '?updateMask.fieldPaths-key',
+                url: baseURL + '/' + key,
                 contentType: 'application/json',
                 processData: false,
-                dataType: 'json',
                 data: JSON.stringify({
                     //"name": val['coffee'],
                     'fields': {
@@ -31,7 +27,7 @@
                             'mapValue': {
                                 'fields': {
                                     'coffee': {'stringValue': val['coffee']},
-                                    'email': {'stringValue': val['emailAddress']},
+                                    'emailAddress': {'stringValue': val['emailAddress']},
                                     'flavor': {'stringValue': val['flavor']},
                                     'size': {'stringValue': val['size']},
                                     'strength': {'stringValue': val['strength']}
@@ -40,11 +36,7 @@
                         }
                     }
                 }),
-
                 success: function(serverResponse) {console.log(serverResponse)},
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader('Authorization', 'Bearer ' + idToken)
-                }
             });
         };
         //--------------
@@ -65,13 +57,6 @@
         }
         //--------------
     }
-
-    RemoteDataStore.prototype.setID = function(api) {
-        $.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + api, {'returnSecureToken': true}, function(serverResponse) {
-            idToken = serverResponse['idToken'];
-        });
-    };
-
     App.RemoteDataStore = RemoteDataStore;
     window.App = App;
     
